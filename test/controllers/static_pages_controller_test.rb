65305c53ba9b1d4a50166f848e1eeabcfcff02a6
Yuuki Kakimoto <kakimoto.yuuki@moneyforward.co.jp>
2018-08-17 02:45:10 +0000

    不要なテストの削除

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -12,12 +12,6 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "#{@base_title}"
   end
 
-  test "should get home" do
-    get static_pages_home_url
-    assert_response :success
-    assert_select "title", "#{@base_title}"
-  end
-
   test "should get help" do
     get static_pages_help_url
     assert_response :success
