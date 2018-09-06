d7859020389f53a610c92f297091967d06bfde2c
Yuuki Kakimoto <kakimoto.yuuki@moneyforward.co.jp>
2018-09-06 17:04:16 +0900

    テスト時のエラーからseed.rbを修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ User.create!(name:  "Example User",
              password:              "foobar",
              password_confirmation: "foobar",
              admin: true,
-             activated: true
+             activated: true,
              activated_at: Time.zone.now)
 
 99.times do |n|
