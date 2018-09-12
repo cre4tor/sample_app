3daab73160c7386d0717ff400b5acc3e5d783965
Yuuki Kakimoto <kakimoto.yuuki@moneyforward.co.jp>
2018-09-12 10:23:07 +0900

    Micropostモデルに画像を追加する

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,6 +2,7 @@ class Micropost < ApplicationRecord
   #マイクロポストがユーザーに所属する (belongs_to) 関連付け
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
+  mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
 end
